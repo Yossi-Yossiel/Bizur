87ae44bacb886990c6a57fa824b26fa679b6ab48
Aviv Avichail <avivavichail@gmail.com>
2022-09-20 12:23:51 +0300

    worked on the server

--- Bizur_server.py
+++ Bizur_server.py
@@ -1,6 +1,14 @@
 import socket
 import hashlib
 
+
+def distribute(client: socket, count: int, hashcode: str):
+    client.send((str(count) + " " + str(count+10000000) + " " + hashcode).encode())
+    while True:
+        d = client.recv(1024).decode()
+
+
+hashcode = "EC9C0F7EDCC18A98B1F31853B1813301"
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('0.0.0.0', 8200))
 clients = []
