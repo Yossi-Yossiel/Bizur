c3d60195576703ed87f97a3cd9e68dc057976051
Aviv Avichail <avivavichail@gmail.com>
2022-10-02 12:23:57 +0300

    worked on the server

--- Bizur_Client.py
+++ Bizur_Client.py
@@ -4,43 +4,47 @@ import threading
 import hashlib
 
 
-def brutehack(min,max, hash, sock):
+def brutehack(min, max, hash, sock):
+    print(str(min) + " " + str(max))
+    print(hash)
     for i in range(min,max+1):
         h = hashlib.md5(str(i).encode()).hexdigest()
         if h == hash:
+            print("found " + str(i))
             sock.send(("found " + str(i)).encode())
 
 
 
 def main():
     coresnum = multiprocessing.cpu_count()
-    port = int(input("port:"))
+    port = 8200
     ip = input("ip:")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ip, port))
-    char = int(sock.recv(1024))
+    char = int(sock.recv(1024).decode())
+    sock.send(("got the char length " + str(char)).encode())
     data = sock.recv(char).decode()  # should receive 3 params: min, max and hash code
+    print(data)
     sock.send("ok".encode())
-    char = int(sock.recv(1024))
     d1 = sock.recv(char).decode()
     if d1 == data:
-        sock.send("good")
+        sock.send("good".encode())
         data = data.split()
-        min = data[0]
-        max = data[1]
+        min = int(data[0])
+        max = int(data[1])
         hashcode = data[2]
     else:
         sock.send("send again".encode())
         d2 = sock.recv(char)
         if d2 == d1:
             sock.send("good".encode())
-            min = d1[0]
-            max = d1[1]
+            min = int(d1[0])
+            max = int(d1[1])
             hashcode = d1[2]
         elif d2 == data:
-            sock.send("good")
-            min = data[0]
-            max = data[1]
+            sock.send("good".encode())
+            min = int(data[0])
+            max = int(data[1])
             hashcode = data[2]
         else:
             sock.send("nope".encode())
@@ -49,11 +53,12 @@ def main():
             hashcode = "Error"
             sock.close()
     mmlist = []
+    print(min)
     mmlist.append(min)
     for i in range(coresnum):
-        n = max // coresnum + (max//coresnum)*i
-        mmlist.append(n)
-
+        n = max-min // coresnum + (max-min//coresnum)
+        mmlist.append(n+min)
+    print(mmlist)
     tlist = []
     for i in range(len(mmlist)-1):
         t = threading.Thread(target=brutehack, args=(mmlist[i],mmlist[i+1],hashcode,sock))
@@ -61,6 +66,7 @@ def main():
         tlist.append(t)
     for i in tlist:
         i.join()
+
     sock.close()
 
 
--- Bizur_server.py
+++ Bizur_server.py
@@ -1,37 +1,58 @@
 import socket
 import hashlib
+import threading
+
+
+def ThreadListen(client: socket.socket):
+    flag = client.recv(1024).decode().split()
+    if flag[0] == "found":
+        print(flag)
+        return
 
 
 def distribute(client: socket.socket, count: int, hashcode: str):
-    sendc = str(count) + " " + str(count+10000000) + " " + hashcode
-    client.send(str((len(sendc))))
-    client.send(sendc.encode())
+    count1 = count + 10000000
+    send_c = str(count) + " " + str(count1) + " " + hashcode
+    length = str(len(send_c))
+    print(length)
+    client.send(length.encode())
+    print(client.recv(1024).decode())
+    client.send(send_c.encode())
     d = client.recv(1024).decode()
     if d == "ok":
-        client.send(sendc.encode())
+        client.send(send_c.encode())
         d = client.recv(1024).decode()
         if d == "good":
-            return True
+            return True,count
         elif d == "send again":
-            client.send(sendc.encode())
+            client.send(send_c.encode())
             d = client.recv(1024)
             if d == "good":
-                return True
+                return True,count
             else:
-                return False
+                return False,count
 
 
-hashcode = "EC9C0F7EDCC18A98B1F31853B1813301"
+hashcode = hashlib.md5("1000000010".encode()).hexdigest() #"EC9C0F7EDCC18A98B1F31853B1813301".lower()
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('0.0.0.0', 8200))
 clients = []
 count = 1000000000
 i = 0
+tlist = []
 while True:
     sock.listen()
     clients.append(sock.accept())
     x = clients[i]
+    client = x[0]
     print("client " + str(x[1]) + " decided to be a black slave")
-    if not distribute():
+    workFlag,count = distribute(client,count,hashcode)
+    if not workFlag:
+        print("could not send the info, closing connection")
+        client.close()
+    t = threading.Thread(target=ThreadListen, args=(client,))
+    t.start()
+    tlist.append(t)
 
+    print(workFlag)
     i += 1
